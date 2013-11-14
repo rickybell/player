ef3614a30f889190e57d8f82311fef85d6bf5c95
rickybell <henrique.belloube@gmail.com>
2013-11-14 00:10:52 -0200

    adding 'rails' project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Player::Application.config.secret_token = '33105643f3b289d569676b633756c59b13ee6c945e7af36021d88fa57db968e6c757c33ccb89fb32b0a4ea103fec67c3f3bb1bbf8899da38d05503b7d80d06a8'
